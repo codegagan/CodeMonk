f9a5bfdcc9936b1d0968af461fa95c852c71dff6
Gagandeep Singh <codegagan@gmail.com>
2016-11-03 23:55:00 +0530

    restructured the package

--- src/com/gagan/hackerearth/codemonk/arrayandstrings/challenge/MonkAndWelcome.java
+++ src/com/gagan/hackerearth/codemonk/arrayandstrings/challenge/MonkAndWelcome.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.gagan.codemonk.arrayandstrings;
+package com.gagan.hackerearth.codemonk.arrayandstrings.challenge;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
